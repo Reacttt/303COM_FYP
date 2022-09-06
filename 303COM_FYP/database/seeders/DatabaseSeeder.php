c3d697e3b6eddca1d640aeea793f2cb833c851e1
Reacttt <47073160+Reacttt@users.noreply.github.com>
2022-09-06 23:46:21 +0800

    Implemented Seeder for Product

--- 303COM_FYP/database/seeders/DatabaseSeeder.php
+++ 303COM_FYP/database/seeders/DatabaseSeeder.php
@@ -28,5 +28,9 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class
         ]);
+
+        $this->call([
+            ProductSeeder::class
+        ]);
     }
 }
---
+++ 303COM_FYP/database/seeders/ProductSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $category_id_list = DB::table('category')->pluck('category_id');
+        for ($i = 0; $i < 6; $i++) {
+            $products[] = [
+                'category_id' => rand(1,6),
+                'product_name' => 'product name #' . $i+1,
+                'product_description' => 'product description for ' . $i+1,
+                'product_image' => 'product-' . $i+1 . '.jpg',
+                'product_stock' => rand(5,50),
+                'product_price' => rand(5,50) . '.99',
+                'product_status' => '1'
+            ];
+        }
+
+        DB::table('product')->insert($products);
+    }
+}
