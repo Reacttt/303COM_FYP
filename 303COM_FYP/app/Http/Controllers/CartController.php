ed7845965ec6c676114f6975036f2e3b25766811
Reacttt <47073160+Reacttt@users.noreply.github.com>
2022-09-08 22:18:28 +0800

    Implemented Validation for Duplicate Cart Item

--- 303COM_FYP/app/Http/Controllers/CartController.php
+++ 303COM_FYP/app/Http/Controllers/CartController.php
@@ -13,14 +13,32 @@ class CartController extends Controller
 
         $user_id = DB::table('user')->where('user_username', $user_username)->value('user_id');
 
-        $data = array(
-            "user_id" => $user_id,
-            "product_id" => $product_id,
-            "product_quantity" => $quantity,
-        );
+        $result = DB::select('select * from cart where user_id = ? AND product_id = ?', [$user_id, $product_id]);
 
-        DB::table('cart')->insert($data);
+        // Create New Cart Item if product not duplicate in user cart
+        if ($result == null) {
+            $data = array(
+                "user_id" => $user_id,
+                "product_id" => $product_id,
+                "product_quantity" => $quantity,
+            );
 
-        return redirect('product')->with('alert', 'Product added to cart!');
+            DB::table('cart')->insert($data);
+
+            return redirect('product')->with('alert', 'Product added to cart!');
+        } else if ($result) {
+
+            $product_quantity = DB::table('cart')->where('user_id', $user_id)->where('product_id', $product_id)->value('product_quantity');
+
+            $newQuantity = $product_quantity + $quantity;
+
+            $data = array(
+                "product_quantity" => $newQuantity,
+            );
+
+            DB::table('cart')->where('user_id', $user_id)->where('product_id', $product_id)->update($data);
+
+            return redirect('product')->with('alert', 'Product quantity updated in cart!');
+        }
     }
 }
