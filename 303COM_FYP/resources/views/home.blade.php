a9b511ca2e4966f4dd2fd95254ad4696dba59c23
Reacttt <47073160+Reacttt@users.noreply.github.com>
2022-09-11 02:29:37 +0800

    Optimized All Pages Performance

--- 303COM_FYP/resources/views/home.blade.php
+++ 303COM_FYP/resources/views/home.blade.php
@@ -14,12 +14,6 @@
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400|Source+Code+Pro:700,900&display=swap" rel="stylesheet">
 
-    <!-- CSS Libraries -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-    <link href="lib/slick/slick.css" rel="stylesheet">
-    <link href="lib/slick/slick-theme.css" rel="stylesheet">
-
     <!-- Template Stylesheet -->
     <link rel="stylesheet" href="<?php echo asset('css/home.css') ?>" type="text/css">
 </head>
@@ -232,14 +226,6 @@
 
 @include('footer')
 
-<!-- JavaScript Libraries -->
-<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-<script src="lib/easing/easing.min.js"></script>
-<script src="lib/slick/slick.min.js"></script>
-
-<!-- Template Javascript -->
-<script src="js/main.js"></script>
 </body>
 
 </html>
