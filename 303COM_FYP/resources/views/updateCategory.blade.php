1c4f63efd57c02f9d78f1518ca032b045723cd3c
Reacttt <47073160+Reacttt@users.noreply.github.com>
2022-09-11 01:07:19 +0800

    Update Update Category Interface

--- 303COM_FYP/resources/views/updateCategory.blade.php
+++ 303COM_FYP/resources/views/updateCategory.blade.php
@@ -44,7 +44,7 @@
                <div class="col-sm-4">
                   <div class="page-header float-left">
                      <div class="page-title">
-                        <h1><b>Update Product</b></h1>
+                        <h1><b>Update Category</b></h1>
                      </div>
                   </div>
                </div>
