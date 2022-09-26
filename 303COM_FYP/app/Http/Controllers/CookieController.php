e47fe18c822b42289d72c0c66e95fcda62a6ccdc
Reacttt <47073160+Reacttt@users.noreply.github.com>
2022-09-27 01:31:35 +0800

    Updated CookieController

--- 303COM_FYP/app/Http/Controllers/CookieController.php
+++ 303COM_FYP/app/Http/Controllers/CookieController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class CookieController extends Controller
 {
     public function updateCookie($type = null, $value = null) {
             setcookie($type, $value, '0', '/');
+
          return back()->with('alert', 'Updated ' . $type . ' to ' . $value);
     }
 }
