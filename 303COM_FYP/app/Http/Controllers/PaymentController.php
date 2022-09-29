caa47a4329dd1e898d979597ac2c645f5a6499d8
Reacttt <47073160+Reacttt@users.noreply.github.com>
2022-09-29 21:47:42 +0800

    Removed Update Payment Status

--- 303COM_FYP/app/Http/Controllers/PaymentController.php
+++ 303COM_FYP/app/Http/Controllers/PaymentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Http;
 
 class PaymentController extends Controller
 {
@@ -15,23 +16,23 @@ class PaymentController extends Controller
         if ($request->payment_method != "Crypto") $payment_transaction = Hash::make('transaction');
         else $payment_transaction = $request->payment_transaction;
 
-            // Credit Card Validation
-            // $this->validate($request, [  
-            //     'cc_name' => 'required',
-            //     'cc_number' => 'required',
-            //     'cc_exp' => 'required',
-            //     'cc_code' => 'required',
-            // ]);
+        // Credit Card Validation
+        // $this->validate($request, [  
+        //     'cc_name' => 'required',
+        //     'cc_number' => 'required',
+        //     'cc_exp' => 'required',
+        //     'cc_code' => 'required',
+        // ]);
 
-            $data = array(
-                "order_id" => $request->order_id,
-                "payment_total" => $request->payment_amount,
-                "payment_method" => $request->payment_method,
-                "payment_currency" => $request->payment_currency,
-                "payment_transaction" => $payment_transaction,
-                "payment_status" => 1,
-                "created_at" => $created_at
-            );
+        $data = array(
+            "order_id" => $request->order_id,
+            "payment_total" => $request->payment_amount,
+            "payment_method" => $request->payment_method,
+            "payment_currency" => $request->payment_currency,
+            "payment_transaction" => $payment_transaction,
+            "payment_status" => $request->payment_status,
+            "created_at" => $created_at
+        );
 
         DB::table('payment_details')->insert($data);
 
@@ -42,16 +43,9 @@ class PaymentController extends Controller
 
         DB::table('order')->where('order_id', $request->order_id)->update($data);
 
-        return redirect('order')->with('alert', $request->payment_method . ' Payment Successful!');
-    }
-
-    public function addMetamaskTransaction(Request $request)
-    {
-        $data = array(
-            "txHash" => $request->txHash,
-            "amount" => $request->amount,
-        );
-
-        return redirect('/')->with('alert', 'Metamask Payment Successful! txHash:' . $request->txHash . " Amount: " . $request->amount);
+        if ($request->payment_method != "Crypto")
+            return redirect('order')->with('alert', $request->payment_method . ' Payment Successful!');
+        else
+            return redirect('order')->with('alert', $request->payment_method . ' Payment Submitted Successful!');
     }
 }
