0769359a8ff7dfc5dd0c79371ed975cd6851440d
Reacttt <47073160+Reacttt@users.noreply.github.com>
2022-10-18 00:51:39 +0800

    Removed Token Payment Options

--- 303COM_FYP/resources/views/payment.blade.php
+++ 303COM_FYP/resources/views/payment.blade.php
@@ -236,19 +236,11 @@
                                  <input type="text" id="crypto_amount" name="payment_amount" class="form-control" value="{{ $crypto_grandTotal }}" readonly>
                               </div>
                               <div>
-                                 @if ($crypto_currency != "ETH")
-                                 <button type="button" onClick="" class="btn btn-lg btn-info btn-block">
-                                    <i class="fa fa-lock"></i>&nbsp;
-                                    <span>Pay {{ $crypto_grandTotal }} {{ $crypto_currency }} (Token)</span>
-                                    <span style="display:none;">Sending…</span>
-                                 </button>
-                                 @else
                                  <button type="button" onClick="startProcess()" class="btn btn-lg btn-info btn-block">
                                     <i class="fa fa-lock"></i>&nbsp;
-                                    <span>Pay {{ $crypto_grandTotal }} {{ $crypto_currency }} (Native)</span>
+                                    <span>Pay {{ $crypto_grandTotal }} {{ $crypto_currency }} </span>
                                     <span style="display:none;">Sending…</span>
                                  </button>
-                                 @endif
                               </div>
                            </form>
                         </div>
@@ -268,12 +260,12 @@
 @include('footer')
 
 <script>
-   // ETH Transaction on Metamask
-
    function startProcess() {
+      var payment_currency = "<?php echo $crypto_currency; ?>";
       if ($('#crypto_amount').val() > 0) {
          // run metamsk functions here
-         EThAppDeploy.loadEtherium();
+         if (payment_currency == 'ETH')
+            EThAppDeploy.loadEtherium();
       } else {
          alert('Please Enter Valid Amount');
       }
@@ -360,63 +352,6 @@
             alert("Crypto Payment Submitted Successfully!");
          }
       });
-   }
-
-   function send_token(
-      contract_address,
-      send_token_amount,
-      to_address,
-      send_account,
-      private_key
-   ) {
-      let wallet = new ethers.Wallet(private_key)
-      let walletSigner = wallet.connect(window.ethersProvider)
-
-      window.ethersProvider.getGasPrice().then((currentGasPrice) => {
-         let gas_price = ethers.utils.hexlify(parseInt(currentGasPrice))
-         console.log(`gas_price: ${gas_price}`)
-
-         if (contract_address) {
-            // general token send
-            let contract = new ethers.Contract(
-               contract_address,
-               send_abi,
-               walletSigner
-            )
-
-            // How many tokens?
-            let numberOfTokens = ethers.utils.parseUnits(send_token_amount, 18)
-            console.log(`numberOfTokens: ${numberOfTokens}`)
-
-            // Send tokens
-            contract.transfer(to_address, numberOfTokens).then((transferResult) => {
-               console.dir(transferResult)
-               alert("sent token")
-            })
-         } // ether send
-         else {
-            const tx = {
-               from: send_account,
-               to: to_address,
-               value: ethers.utils.parseEther(send_token_amount),
-               nonce: window.ethersProvider.getTransactionCount(
-                  send_account,
-                  "latest"
-               ),
-               gasLimit: ethers.utils.hexlify(gas_limit), // 100000
-               gasPrice: gas_price,
-            }
-            console.dir(tx)
-            try {
-               walletSigner.sendTransaction(tx).then((transaction) => {
-                  console.dir(transaction)
-                  alert("Send finished!")
-               })
-            } catch (error) {
-               alert("failed to send!!")
-            }
-         }
-      })
    }
 </script>
 
